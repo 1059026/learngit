0804641aa1dba30181eff2cd877d53567d952e04
rodman <1059026@qq.com>
2019-04-11 17:52:55 +0800

    road-vue commit

---
+++ road-mobile/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import router from './router'
+import App from './App'
+import $ from 'jquery'
+import iView from 'iview'
+import axios from 'axios'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
